f7846c536df611200a55199842b64e641665dbc3
Gabriel Sene <sene300@gmail.com>
2023-11-13 00:02:08 -0300

    first commit

---
+++ src/main/java/br/com/gerenciausers/GerenciaUsersApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciausers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaUsersApplication.class, args);
+	}
+
+}
